81091888bc052df852ba03a94d43a9b70976f0f8
cxxxr <g23tlm@gmail.com>
2024-03-02 18:34:03 +0900

    add quote support

--- lisql.lisp
+++ lisql.lisp
@@ -13,11 +13,15 @@
         :collect (etypecase elt
                    (integer (princ-to-string elt))
                    (string (format nil "'~A'" elt))
+                   ;; (keyword (format nil "\"~A\"" (string-downcase elt)))
                    (symbol elt)
                    (list
-                    (if (equal elt '(now))
-                        "now()"
-                        (format nil "(~A)" (stringify-with-comma elt)))))))
+                    (cond ((equal elt '(now))
+                           "now()")
+                          ((eq (first elt) 'cl:quote)
+                           (format nil "\"~A\"" (string-downcase (second elt))))
+                          (t
+                           (format nil "(~A)" (stringify-with-comma elt))))))))
 
 (defun stringify-with-comma (list)
   (format nil "~{~A~^, ~}"
