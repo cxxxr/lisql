fc272510116ac59419d6a47f7f5d38b2cb682a5b
cxxxr <g23tlm@gmail.com>
2024-03-02 15:04:58 +0900

    fix

--- lisql.lisp
+++ lisql.lisp
@@ -5,16 +5,20 @@
            :update))
 (in-package :lisql)
 
+(defun stringify-list (list)
+  (loop :for elt :in list
+        :collect (etypecase elt
+                   (string (format nil "'~A'" elt))
+                   (symbol elt)
+                   (list (format nil "(~A)" (stringify-with-comma elt))))))
+
 (defun stringify-with-comma (list)
-  (format nil "~{~A~^, ~}" list))
+  (format nil "~{~A~^, ~}"
+          (stringify-list list)))
 
 (defun stringify (list)
   (format nil "~{~A~^ ~}"
-          (loop :for elt :in list
-                :collect (etypecase elt
-                           (string (format nil "'~A'" elt))
-                           (symbol elt)
-                           (list (format nil "(~A)" (stringify-with-comma elt)))))))
+          (stringify-list list)))
 
 (defmacro select (&body args)
   `(stringify '(select ,@args)))
